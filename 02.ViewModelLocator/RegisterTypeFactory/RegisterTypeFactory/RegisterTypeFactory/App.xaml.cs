993e9b5b0d27db9cfd40556e3b759255c645b814
nuits.jp <nuits.jp@live.jp>
2016-08-14 21:33:42 +0900

    RegisterFactory・RegisterTypeFactory追加

---
+++ 02.ViewModelLocator/RegisterTypeFactory/RegisterTypeFactory/RegisterTypeFactory/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+using Prism.Unity;
+using RegisterTypeFactory.ViewModels;
+using RegisterTypeFactory.Views;
+
+namespace RegisterTypeFactory
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+            ViewModelLocationProvider.Register<MainPage, HogeViewModel>();
+        }
+    }
+}
---
+++ 02.ViewModelLocator/RegisterTypeFactory/RegisterTypeFactory/RegisterTypeFactory/ViewModels/HogeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RegisterTypeFactory.ViewModels
+{
+    public class HogeViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public HogeViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
