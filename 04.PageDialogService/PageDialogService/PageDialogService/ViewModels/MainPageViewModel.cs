6c50d2f4c597753772da71011b34c562e4867c45
nuits.jp <nuits.jp@live.jp>
2016-09-04 01:02:05 +0900

    PageDialogService更新

--- 04.PageDialogService/PageDialogService/PageDialogService/ViewModels/MainPageViewModel.cs
+++ 04.PageDialogService/PageDialogService/PageDialogService/ViewModels/MainPageViewModel.cs
@@ -5,6 +5,8 @@ using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PageDialogService.ViewModels
 {
@@ -17,11 +19,33 @@ namespace PageDialogService.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        private IPageDialogService _dialogService;
-
+        private readonly IPageDialogService _pageDialogService;
+        public ICommand DisplayDialogCommand { get; }
+        public ICommand DisplayActionSheetCommand { get; }
         public MainPageViewModel(IPageDialogService pageDialogService)
         {
-            _dialogService = pageDialogService;
+            _pageDialogService = pageDialogService;
+            DisplayDialogCommand = new DelegateCommand(async () =>
+            {
+                var result = await _pageDialogService.DisplayAlertAsync("Title", "何れかを選んでください。", "はい", "いいえ");
+                await _pageDialogService.DisplayAlertAsync("Title", $"選択結果：{result}", "OK");
+            });
+            DisplayActionSheetCommand = new DelegateCommand(async () =>
+            {
+                //var result = await _pageDialogService.DisplayActionSheetAsync("共有先を選択してください。", "キャンセル", "削除", "Twitter", "LINE", "Facebook");
+                //await _pageDialogService.DisplayAlertAsync("Title", $"選択結果：{result}", "OK");
+                var cancelButton = ActionSheetButton.CreateCancelButton("キャンセル", new DelegateCommand(async () => await DisplayAlert("キャンセル")));
+                var deleteButton = ActionSheetButton.CreateDestroyButton("削除", new DelegateCommand(async () => await DisplayAlert("削除")));
+                var twitterButton = ActionSheetButton.CreateButton("Twitter", new DelegateCommand(async () => await DisplayAlert("Twitter")));
+                var lineButton = ActionSheetButton.CreateButton("LINE", new DelegateCommand(async () => await DisplayAlert("LINE")));
+                var facebookButton = ActionSheetButton.CreateButton("Facebook", new DelegateCommand(async () => await DisplayAlert("Facebook")));
+                await _pageDialogService.DisplayActionSheetAsync("共有先を選択してください。", cancelButton, deleteButton, twitterButton, lineButton, facebookButton);
+            });
+        }
+
+        private async Task DisplayAlert(string message)
+        {
+            await _pageDialogService.DisplayAlertAsync("Title", $"選択結果：{message}", "OK");
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
