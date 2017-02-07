1a6b84b0f00e7ddcecd7cb07d7b87f7f0a173a48
nuits.jp <nuits.jp@live.jp>
2017-02-07 13:42:27 +0900

    Add Projects

---
+++ 08.Containers/PrismAutofacApp/PrismAutofacApp/PrismAutofacApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Prism.Autofac;
+using Prism.Autofac.Forms;
+using PrismAutofacApp.ViewModels;
+using PrismAutofacApp.Views;
+
+namespace PrismAutofacApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainPageViewModel>();
+            builder.Update(Container);
+
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
